041641d24be61fd2e9e803d5bede2929b15eb462
ipaullly <ipaully22@gmail.com>
2019-05-27 11:28:47 +0300

    remove redundant console.log statements and rename class to className

--- src/components/editButton.js
+++ src/components/editButton.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 
 const Edit = id => (
   <div className="float-right">
-      {console.log(id)}
     <Link to={`/pets/${id.id}/edit`}>
         <button className="btn btn-outline-info" title="Edit pet" type="button">
             <i className="fas fa-edit" />
